9f643efcd9ff23d1d7b3d9ab48127ab83f37626f
J0na555 <ethiopediaa@gmail.com>
2025-08-21 01:35:45 +0300

    added model and also configured the urls

--- memes/models.py
+++ memes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Memes(models.Model):
+    id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length = 20)
+    url = models.CharField(max_length= 255)
+    upvotes = models.IntegerField(default= 0)
+    downvotes= models.IntegerField(default = 0)
+    created_at = models.DateField(auto_now_add= True)
+
+    def __str__(self):
+        return self.title
