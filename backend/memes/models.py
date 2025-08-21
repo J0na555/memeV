69d71a859e554a489e4dfcebb74f5f0bdd88d305
J0na555 <ethiopediaa@gmail.com>
2025-08-22 01:10:54 +0300

    modified the model and the voting system

--- backend/memes/models.py
+++ backend/memes/models.py
@@ -1,15 +1,35 @@
+from django.contrib.auth.admin import User
 from django.db import models
+from django.contrib.auth.models import User
 
 class Memes(models.Model):
     id = models.AutoField(primary_key = True)
     title = models.CharField(max_length = 20)
     url = models.CharField(max_length= 255)
-    upvotes = models.IntegerField(default= 0)
-    downvotes= models.IntegerField(default = 0)
+    # upvotes = models.IntegerField(default= 0)
+    # downvotes= models.IntegerField(default = 0)
     created_at = models.DateField(auto_now_add= True)
 
-    class Meta:
-        ordering = ['upvotes', 'downvotes', 'title']
+    def upvotes(self):
+        return self.votes.filter(vote_type= 'UP').count()
+    
+    def downvotes(self):
+        return self.votes.filter(vote_type= 'DOWN').count()
 
-    def __str__(self):
-        return self.title
+
+    # class Meta:
+    #     ordering = ['upvotes', 'downvotes', 'title']
+
+    # def __str__(self):
+    #     return self.title
+
+class vote(models.Model):
+    VOTE_TYPES = [
+        ('UP', 'Upvote'),
+        ('DOWN', 'Downvote')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meme = models.ForeignKey(Memes, related_name= 'votes' , on_delete= models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'meme')
