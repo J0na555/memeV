2db2eea1b4f80273bf1f3ffe06dceefbeb734f20
J0na555 <ethiopediaa@gmail.com>
2025-08-24 18:25:12 +0300

    added base url for home (forgot to add first)

--- backend/memes/urls.py
+++ backend/memes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import meme_list, vote_memes
 
 urlpatterns = [
+    path('', meme_list, name='meme-list'),
     path('api/memes/', meme_list, name='meme-list'),
     path('api/memes/<int:pk>/<str:action>/', vote_memes, name='vote-memes'),
 ]
